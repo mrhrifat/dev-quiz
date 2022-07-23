02e6552e6e334f44b23717c2e5fa6783814d8bae
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-23 16:28:53 +0600

    build: signup component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Form from "./Form";
 import Illustration from "./Illustration";
+import classes from '../styles/Signup.module.css'
 
 const SignUp = () => {
   return (
     <>
       <h1>Create an account</h1>
-      <Illustration />
-      <Form />
+      <div className="column">
+        <Illustration />
+        <Form className={`${classes.signup}`} />
+      </div>
     </>
   );
 };
