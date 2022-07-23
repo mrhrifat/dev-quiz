4111ad38a19390e092affe79b8679d31002f9244
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-23 15:00:55 +0600

    build: sign up component

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Form from "./Form";
+import Illustration from "./Illustration";
+
+const SignUp = () => {
+  return (
+    <>
+      <h1>Create an account</h1>
+      <Illustration />
+      <Form />
+    </>
+  );
+};
+
+export default SignUp;
