a05d70c81a205076081bdb8e8e549758a29dda88
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-23 16:50:35 +0600

    build: Login form component

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "../styles/Form.module.css";
+import Button from "./Button";
+import TextInput from "./TextInput";
+
+const LoginForm = ({ className, children, ...rest }) => {
+  return (
+    <form className={`${className} ${classes.form}`} action="#">
+      <TextInput
+        placeholder="Enter email"
+        type="email"
+        icon="alternate_email"
+      />
+      <TextInput placeholder="Enter password" type="password" icon="lock" />
+      <Button>Login</Button>
+      <div class="info">Don't have an account? <a href="signup.html">Signup</a> instead.</div>
+    </form>
+  );
+};
+
+export default LoginForm;
