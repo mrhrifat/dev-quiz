94c6c009f4de91305844bd76eb14a48b0319d333
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-23 16:28:21 +0600

    build: form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import classes from "../styles/Form.module.css";
+import Button from "./Button";
+import CheckBox from "./CheckBox";
+import TextInput from "./TextInput";
 
-const Form = () => {
+const Form = ({ children, className, ...rest }) => {
   return (
-    <div>Form</div>
-  )
-}
+    <form className={`${className} ${classes.form}`} action="#" {...rest}>
+      <TextInput placeholder="Enter name" type="text" icon="person" />
+      <TextInput
+        placeholder="Enter email"
+        type="email"
+        icon="alternate_email"
+      />
+      <TextInput placeholder="Enter password" type="password" icon="lock" />
+      <TextInput
+        placeholder="Confirm password"
+        type="password"
+        icon="lock_clock"
+      />
+      <CheckBox text="I agree to the Terms &amp; Conditions" />
+      <Button>Submit now</Button>
+      <div className="info">
+        Already have an account? <a href="login.html">Login</a> instead.
+      </div>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
