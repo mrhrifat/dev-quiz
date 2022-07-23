835754aa8a442bfb4cfc50b9ff3da4c6295d9d9d
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-23 16:58:27 +0600

    build: pass dynamic illustration

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import classes from "../styles/Login.module.css";
 import Illustration from "./Illustration";
 import LoginForm from "./LoginForm";
+import login from "../images/login.svg";
 
 const Login = () => {
   return (
     <>
       <h1>Login to your account</h1>
       <div className="column">
-        <Illustration />
+        <Illustration illustration={login} />
         <LoginForm className={classes.login} />
       </div>
     </>
