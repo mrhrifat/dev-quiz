bad3c3ce4f1666bfec0aafacbc8791c7274878bc
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-23 14:49:28 +0600

    build: video component

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import qs from "../images/3.jpg";
+import classes from "../styles/Video.module.css";
 
 const Video = () => {
   return (
-    <div>Video</div>
-  )
-}
+    <a href="quiz.html">
+      <div className={classes.video}>
+        <img src={qs} alt="" />
+        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+        <div className={classes.qmeta}>
+          <p>10 Questions</p>
+          <p>Score : Not taken yet</p>
+        </div>
+      </div>
+    </a>
+  );
+};
 
-export default Video
+export default Video;
