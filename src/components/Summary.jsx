521d8cc0c703588a90e9c3a00c37d6581a916f93
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 12:53:56 +0600

    build: summary component

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import success from "../images/success.png";
+import classes from "../styles/Summary.module.css";
 
 const Summary = () => {
   return (
-    <div>Summary</div>
-  )
-}
+    <div className={classes.summary}>
+      <div className={classes.point}>
+        <p className={classes.score}>
+          Your score is <br />5 out of 10
+        </p>
+      </div>
 
-export default Summary
+      <div className={classes.badge}>
+        <img src={success} alt="Success" />
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
