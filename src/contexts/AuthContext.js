89ed47145a134283c1cb6ed0beeceb1354ba878d
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 16:11:23 +0600

    build: auth context

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,77 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
+
+const AuthContext = React.createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+const AuthProvider = ({ childern }) => {
+  const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  // SignUp
+  const signup = async (email, password, username) => {
+    const auth = getAuth();
+    await createUserWithEmailAndPassword(auth, email, password);
+
+    // Update Profile
+    await updateProfile(auth.currentUser, {
+      displayName: username,
+    });
+
+    // Update Local State
+    const user = auth.currentUser;
+
+    setCurrentUser({
+      ...user,
+    });
+  };
+
+  // Login
+  const login = (email, password) => {
+    const auth = getAuth();
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Logout
+  const logout = () => {
+    const auth = getAuth();
+    return signOut(auth);
+  };
+
+  // Value
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && childern}
+    </AuthContext.Provider>
+  );
+};
+
+
+export default AuthProvider
