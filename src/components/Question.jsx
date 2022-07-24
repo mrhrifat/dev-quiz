19286de705a6a6b99c130e42114c6a1cb10d04b2
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 12:58:39 +0600

    build: quiestion component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import classes from "../styles/Question.module.css";
+import Answers from "./Answers";
 
 const Question = () => {
   return (
-    <div>Question</div>
-  )
-}
+    <div className={classes.question}>
+      <div className={classes.qtitle}>
+        <span className="material-icons-outlined"> help_outline </span>
+        Here goes the question from Learn with Sumit?
+      </div>
+      <Answers />
+    </div>
+  );
+};
 
-export default Question
+export default Question;
