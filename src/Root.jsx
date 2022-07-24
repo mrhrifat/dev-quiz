ad0e49b14c80676fdd690edca46760d2d1af4838
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 14:09:45 +0600

    build: route

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-// import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 import Layout from "./components/Layout";
+import Login from "./components/Login";
+import Quiz from "./components/Quiz";
 import Result from "./components/Result";
-// import Quiz from "./components/Quiz";
-// import Login from "./components/Login";
-// import SignUp from "./components/SignUp";
+import SignUp from "./components/SignUp";
 
 const Root = () => {
   return (
-    <Layout>
-      {/* <Home /> */}
-      {/* <SignUp /> */}
-      {/* <Login/> */}
-      {/* <Quiz /> */}
-      <Result />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 };
 
