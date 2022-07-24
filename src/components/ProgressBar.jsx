b556e313d1e7ecd40b8f0f32424f8883d639b2f8
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 14:21:40 +0600

    build: route link

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "../styles/ProgressBar.module.css";
 import Button from "./Button";
 
@@ -14,12 +15,12 @@ const ProgressBar = () => {
           <div className={classes.progress} style={{ width: "20%" }}></div>
         </div>
       </div>
-      <a href="result.html">
+      <Link to="/result">
         <Button className={`${classes.button} ${classes.next}`}>
           <span>Next Question</span>
           <span className="material-icons-outlined"> arrow_forward </span>
         </Button>
-      </a>
+      </Link>
     </div>
   );
 };
