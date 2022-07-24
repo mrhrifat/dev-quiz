6e1f4e37568736f99da8d9698bf85e9cdcba5742
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 12:34:38 +0600

    build: progressbar component

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import classes from "../styles/ProgressBar.module.css";
+import Button from "./Button";
 
 const ProgressBar = () => {
   return (
-    <div>ProgressBar</div>
-  )
-}
+    <div className={classes.progressBar}>
+      <div className={classes.backButton}>
+        <span className="material-icons-outlined"> arrow_back </span>
+      </div>
+      <div className={classes.rangeArea}>
+        <div className={classes.tooltip}>24% Cimplete!</div>
+        <div className={classes.rangeBody}>
+          <div className={classes.progress} style={{ width: "20%" }}></div>
+        </div>
+      </div>
+      <a href="result.html">
+        <Button className={`${classes.button} ${classes.next}`}>
+          <span>Next Question</span>
+          <span className="material-icons-outlined"> arrow_forward </span>
+        </Button>
+      </a>
+    </div>
+  );
+};
 
-export default ProgressBar
+export default ProgressBar;
