f82113003f6b7b950d6744430fe0ff74355bbc91
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 12:43:46 +0600

    build: mini player component

--- src/components/MiniPlayer.jsx
+++ src/components/MiniPlayer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import image from "../images/3.jpg";
+import classes from "../styles/MiniPlayer.module.css";
 
 const MiniPlayer = () => {
   return (
-    <div>MiniPlayer</div>
-  )
-}
+    <div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
+      <span className={`material-icons-outlined ${classes.open}`}>
+        play_circle_filled
+      </span>
+      <span className={`material-icons-outlined ${classes.close}`}>close</span>
+      <img src={image} alt={image} />
+      <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+    </div>
+  );
+};
 
-export default MiniPlayer
+export default MiniPlayer;
