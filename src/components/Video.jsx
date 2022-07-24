f63a2d5b1b8a6783417dad6decaa21440eae4eb9
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 14:21:45 +0600

    build: route link

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import qs from "../images/3.jpg";
 import classes from "../styles/Video.module.css";
 
 const Video = () => {
   return (
-    <a href="quiz.html">
+    <Link to="/quiz">
       <div className={classes.video}>
         <img src={qs} alt="" />
         <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
@@ -13,7 +14,7 @@ const Video = () => {
           <p>Score : Not taken yet</p>
         </div>
       </div>
-    </a>
+    </Link>
   );
 };
 
