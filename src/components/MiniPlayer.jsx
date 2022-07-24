88ceb57d460f3231f6207449aa270de6e211395c
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 12:19:50 +0600

    build: funcational component

---
+++ src/components/MiniPlayer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MiniPlayer = () => {
+  return (
+    <div>MiniPlayer</div>
+  )
+}
+
+export default MiniPlayer
---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProgressBar = () => {
+  return (
+    <div>ProgressBar</div>
+  )
+}
+
+export default ProgressBar
