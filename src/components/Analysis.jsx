34a36b4c0d8bbd75046a448fba687393f309d20a
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-24 12:55:10 +0600

    build: analysis component

--- src/components/Analysis.jsx
+++ src/components/Analysis.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import classes from "../styles/Analysis.module.css";
+import Question from "./Question";
 
 const Analysis = () => {
   return (
-    <div>Analysis</div>
-  )
-}
+    <div className={classes.analysis}>
+      <h1>Question Analysis</h1>
+      <h4>You answerd 5 out of 10 questions correctly</h4>
+      <Question />
+    </div>
+  );
+};
 
-export default Analysis
+export default Analysis;
