371193f6bb36a62cf041ae90be18f0521ccba7fd
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-26 12:21:18 +0600

    build: login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,21 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 import classes from "../styles/Form.module.css";
 import Button from "./Button";
 import TextInput from "./TextInput";
 
 const LoginForm = ({ className, children, ...rest }) => {
+  const { login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+      await login(email, password);
+      navigate("/");
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+      setError("Faild to login!");
+    }
+  };
+
   return (
-    <form className={`${className} ${classes.form}`} action="#">
+    <form className={`${className} ${classes.form}`} onSubmit={handleSubmit}>
       <TextInput
         placeholder="Enter email"
         type="email"
         icon="alternate_email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <TextInput placeholder="Enter password" type="password" icon="lock" />
-      <Button>
+      <TextInput
+        placeholder="Enter password"
+        type="password"
+        icon="lock"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit" disabled={loading}>
         <span>Login</span>
       </Button>
+
+      {error && <span className="error">{error}</span>}
+
       <div className={classes.info}>
         Don't have an account? <Link to="/signup">Signup</Link> instead.
       </div>
