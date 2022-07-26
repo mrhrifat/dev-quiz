c039d90ef02382498eb0271a7ec7ac6e14f42864
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-26 13:58:23 +0600

    build: mute old & create 1st way to private auth

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+// import React from "react";
+// import { Route, useNavigate } from "react-router-dom";
+// import { useAuth } from "../contexts/AuthContext";
 
-const PrivateRoute = () => {
-  return (
-    <div>PrivateRoute</div>
-  )
-}
+// const PrivateRoute = ({ component: Component, ...rest }) => {
+//   const { currentUser } = useAuth();
+//   const navigate = useNavigate();
 
-export default PrivateRoute
+//   return currentUser ? (
+//     <Route {...rest}>{(props) => <Component {...props} />}</Route>
+//   ) : (
+//     navigate("/login")
+//   );
+// };
+
+// export default PrivateRoute;
+
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+const PrivateRoute = ({ children }) => {
+  const { currentUser } = useAuth();
+
+  return currentUser ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
