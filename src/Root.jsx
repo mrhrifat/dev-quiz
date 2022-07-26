bf728677bb62597736d21a73bafda7108254858a
Mrh Rifat <mrhrifat383759@gmail.com>
2022-07-26 14:42:03 +0600

    build: private outlet route

--- src/Root.jsx
+++ src/Root.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Layout from "./components/Layout";
 import Login from "./components/Login";
+import PrivateOutlet from "./components/PrivateOutlet";
 import Quiz from "./components/Quiz";
 import Result from "./components/Result";
 import SignUp from "./components/SignUp";
@@ -17,8 +18,10 @@ const Root = () => {
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/result" element={<Result />} />
+            <Route path="/*" element={<PrivateOutlet />}>
+              <Route path="quiz" element={<Quiz />} />
+              <Route path="result" element={<Result />} />
+            </Route>
           </Routes>
         </Layout>
       </AuthProvider>
