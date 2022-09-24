92d4abefb280b2f91fc51ef1bae06a48d8a58910
Mrh Rifat <mrhrifat383759@gmail.com>
2022-09-24 15:33:38 +0600

    build: database url

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,6 +9,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
 
 // Initialize Firebase
